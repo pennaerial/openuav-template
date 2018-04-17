4f89eedef92738c2889142e8444aaa7513cf667a
Edward Atter <atter@seas.upenn.edu>
2018-04-17 10:46:07 -0400

    Trim directory structure

--- run_this.sh
+++ run_this.sh
@@ -9,15 +9,15 @@ source ~/catkin_ws/devel/setup.bash
 ## world setup #
 cp /simulation/inputs/models/empty.world /root/src/Firmware/Tools/sitl_gazebo/worlds/empty.world
 cp /simulation/inputs/models/f450-1/f450-1.sdf /root/src/Firmware/Tools/sitl_gazebo/models/f450-1/f450-1.sdf
-cp /simulation/inputs/setup/posix_sitl_openuav_swarm_base.launch /root/src/Firmware/launch/posix_sitl_openuav_swarm_base.launch
+cp /simulation/inputs/utils/posix_sitl_openuav_swarm_base.launch /root/src/Firmware/launch/posix_sitl_openuav_swarm_base.launch
 
 rm -r /simulation/outputs
 mkdir -p /simulation/outputs 
 echo "Setup..." >> /tmp/debug
 
-python /simulation/inputs/setup/gen_gazebo_ros_spawn.py $num_uavs
-python /simulation/inputs/setup/gen_px4_sitl.py $num_uavs
-python /simulation/inputs/setup/gen_mavros.py $num_uavs
+python /simulation/inputs/utils/gen_gazebo_ros_spawn.py $num_uavs
+python /simulation/inputs/utils/gen_px4_sitl.py $num_uavs
+python /simulation/inputs/utils/gen_mavros.py $num_uavs
 
 # PAR LIBRARY
 echo "PAR Library" >> /tmp/debug
@@ -51,7 +51,7 @@ done
 rosrun web_video_server web_video_server _port:=80 _server_threads:=100 &> /dev/null &
 roslaunch rosbridge_server rosbridge_websocket.launch ssl:=false &> /dev/null &
 
-python /simulation/inputs/setup/testArmAll.py $num_uavs &> /dev/null &
+python /simulation/inputs/utils/testArmAll.py $num_uavs &> /dev/null &
 
 sleep 3
 
@@ -63,7 +63,7 @@ do
 done
 
 echo "Measures..."
-python /simulation/inputs/measures/measureInterRobotDistance.py $num_uavs 1 &> /dev/null &
+python /simulation/inputs/utils/measureInterRobotDistance.py $num_uavs 1 &> /dev/null &
 roslaunch opencv_apps general_contours.launch  image:=/uav_2_camera_front/image_raw debug_view:=false &> /dev/null &
 
 for((i=1;i<=$num_uavs;i+=1))
